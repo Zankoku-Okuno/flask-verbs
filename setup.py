9b5ee175d2a51a9e572255af9af4304010b78c05
Eric Demko <edemko@newfields.com>
2014-09-12 12:39:02 -0400

    oops, don't know what I'm doing with this flask extension thing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Zankoku Okuno',
     author_email='zankoku.okuno@gmail.com',
-    packages=['flask.ext'],
+    packages=['flask_verbs'],
     url='https://github.com/Zankoku-Okuno/flask-verbs',
     license='LICENSE',
     description='Extend Flask with nice syntax for working with HTTP methods (a special case of REST verbs)',
